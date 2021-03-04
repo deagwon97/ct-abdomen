2652d8f706904838a0fa05f6d7e92e0c9b89ae33
Dacon <choiuijin1125@gmail.com>
2021-03-04 17:27:12 +0900

    update

--- access_container.sh
+++ access_container.sh
@@ -5,4 +5,6 @@ docker run --gpus all\
             --mount type=bind,source="/home/dacon/Dacon/HDD_04/ct_1800/1800 데이터(N)",target=/workspace/ct-abdomen/raw_data\
             --mount type=bind,source="/home/dacon/Dacon/HDD_04/ct_1800/1800 데이터(N)/1.원본/00000004",target=/workspace/ct-abdomen/inference_input\
             --mount type=bind,source="/home/dacon/Dacon/HDD_04/data",target=/workspace/ct-abdomen/preprocess_data\
+            --mount type=bind,source="/home/dacon/Dacon/bdg/inference_models",target=/workspace/ct-abdomen/inference_models\
             ct-inf
+            
